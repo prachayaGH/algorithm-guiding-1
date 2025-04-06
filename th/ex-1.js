74733174bf903b61f414540d787fb4df5a1889e5
Prachaya Kosolpattrapk <prachaya.ksp@gmail.com>
2025-04-06 20:15:41 +0700

    algorithm-guiding-1

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,9 +1,21 @@
 const moveZeroes = function (nums) {
-  //Start coding here
-};
+  let left = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      //สลับตำแหน่งให้ไปอยู่ทางซ้าย
+      let temp = nums[left]
+      nums[left] = nums[i]
+      nums[i] = temp
+      left++
+    }
+  }
+  return nums
+}
+
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
 console.log(result1); // [1,3,12,0,0]
 
 const result2 = moveZeroes([0]);
 console.log(result2); // [0]
+
